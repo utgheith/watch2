44814692dc42ec3f34bcfc51139da1b8b8858b9d
Ahmed Gheith <ag@gheith.com>
2021-03-28 10:53:00 -0500

    added files

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "watch"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
+
+libraryDependencies ++= Seq(
+  "com.lihaoyi" %% "os-lib-watch" % "0.7.3"
+)
---
+++ src/main/scala/main.scala
@@ -0,0 +1,51 @@
+import scala.collection.mutable
+
+object main {
+
+  val LOG = false
+
+  def logger(what: String, data: Any) : Unit = {
+    if (LOG) {
+      val d = data match {
+        case a: Array[_] => a.toList.toString
+        case x => s"$x : ${x.getClass.getName}"
+      }
+      println(s"$what $d")
+    }
+  }
+
+  def printer(paths: Set[os.Path]): Unit = {
+    println("event")
+    paths.map(_.relativeTo(os.pwd)).foreach { p => println(s"  $p")}
+  }
+
+  def main(args: Array[String]): Unit = {
+    val data = os.pwd / "data"
+
+
+    os.remove.all(data)
+    os.makeDir(data)
+
+    val changed = mutable.Set[os.Path]()
+
+
+    os.watch.watch(Seq(data), changed.addAll, logger)
+
+    (0 to 1000000).foreach { i =>
+      println(i)
+
+      ('a' to 'z').foreach { n => os.makeDir.all(data / "r" / n.toString) }
+
+      while (changed.size != 27) {
+        Thread.sleep(1)
+      }
+
+      os.remove.all(data / "r")
+
+      while (changed.size != 27) {
+        Thread.sleep(1)
+      }
+    }
+  }
+
+}
