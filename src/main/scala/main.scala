8eeb610bad431ce0c605947b853dde27ba8ed0df
Ahmed Gheith <ag@gheith.com>
2021-03-28 11:37:32 -0500

    print events

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,22 +2,16 @@ import scala.collection.mutable
 
 object main {
 
-  val LOG = false
-
-  def logger(what: String, data: Any) : Unit = {
-    if (LOG) {
-      val d = data match {
-        case a: Array[_] => a.toList.toString
-        case x => s"$x : ${x.getClass.getName}"
-      }
-      println(s"$what $d")
+  def show(what: String, data: Any) : Unit = {
+    val d = data match {
+      case a: Array[_] => a.toList.toString
+      case x => s"$x : ${x.getClass.getName}"
     }
+    println(s"  $what $d")
   }
 
-  def printer(paths: Set[os.Path]): Unit = {
-    println("event")
-    paths.map(_.relativeTo(os.pwd)).foreach { p => println(s"  $p")}
-  }
+  val names = ('a' to 'z').map(_.toString)
+  val n_events = names.size * 2 + 1
 
   def main(args: Array[String]): Unit = {
     val data = os.pwd / "data"
@@ -27,37 +21,42 @@ object main {
     os.makeDir(data)
 
     val changed = mutable.Set[os.Path]()
+    val events = mutable.Buffer[(String,Any)]()
+
+    os.watch.watch(Seq(data), changed.addAll, (w,a) => events.append((w,a)))
 
 
-    os.watch.watch(Seq(data), changed.addAll, logger)
 
     (0 to 1000000).foreach { i =>
       println(i)
 
-      ('a' to 'z').foreach { n =>
-        val s = n.toString
+      names.foreach { s =>
         os.makeDir.all(data / "r" / s)
         os.write(data / "r" / s / s, s)
       }
 
-      while (changed.size != 53) {
+      while (changed.size != n_events) {
         Thread.sleep(1)
       }
       changed.clear()
+      events.clear()
 
       os.remove.all(data / "r")
 
       var iter = 0
-      while ((changed.size != 53) && (iter < 10000)) {
+      while ((changed.size != n_events) && (iter < 10000)) {
         Thread.sleep(1)
         iter += 1
       }
-      if (changed.size != 53) {
+      if (changed.size != n_events) {
         println(s"size = ${changed.size}")
         changed.toList.sorted.foreach(p => println(s" ${p.relativeTo(data)}"))
-        return 
+        println("events")
+        events.foreach { case(e,a) => show(e,a)}
+        return
       }
       changed.clear()
+      events.clear()
     }
   }
 
